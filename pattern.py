1ec19c4bb343d2bc7b8c54eda9244dea1a667c32
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2024-10-09 17:47:56 +0200

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -1,14 +1,14 @@
 select_insert_pattern = re.compile(
     r'''
-    # Match SELECT statements
-    \bSELECT\b.*?\bFROM\s+([A-Za-z0-9_\{\}\(\)\|\.\'\"]+\.[A-Za-z0-9_]+)   # Capture complex database.table pattern
-    (?=\s|WHERE|;|\)|,|\()                                                  # Stop at space, WHERE, ), ;, ,, or (
+    # Capture SELECT statements
+    \bSELECT\b.*?\bFROM\s+([\w\{\}\(\)\|\.\'\"]+\.[A-Za-z0-9_]+)   # Capture full database.table name
+    (?=\s|WHERE|;|\)|,|\()                                         # Stop at space, WHERE, ), ;, ,, or (
 
-    |                                                                       # OR
+    |                                                              # OR
 
-    # Match INSERT statements
-    \bINSERT\s+INTO\s+([A-Za-z0-9_\{\}\(\)\|\.\'\"]+\.[A-Za-z0-9_]+)        # Capture complex database.table pattern
-    (?=\s|\(|;|,)                                                           # Stop at space, (, ;, or ,
+    # Capture INSERT statements
+    \bINSERT\s+INTO\s+([\w\{\}\(\)\|\.\'\"]+\.[A-Za-z0-9_]+)       # Capture full database.table name
+    (?=\s|\(|;|,)                                                  # Stop at space, (, ;, or ,
     ''', 
     re.IGNORECASE | re.VERBOSE | re.DOTALL
 )
